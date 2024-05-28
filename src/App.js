c192bb70e0b2d6937fc7ee08cd597b0aaf4deda9
ksy0725 <80161351+ksy0725@users.noreply.github.com>
2024-05-28 22:06:08 +0900

    Update App.js
    add information of pairing food

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ const App = () => {
         <p className="text-gray-600">
           {sool.description.length > 40 ? `${sool.description.substring(0, 40)}...` : sool.description}
         </p>
+          <p className="text-gray-600"> 추천 음식 : {`${selectedSool.food_pairing}`}</p>
       </div>
     ));
   };
